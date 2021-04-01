2c8b6e149c9ed9b65e930603919a415114efb078
varshapy <81504525+varshapy@users.noreply.github.com>
2021-04-01 15:44:20 +0530

    Update big3.c

--- big3.c
+++ big3.c
@@ -3,7 +3,7 @@ void big3() {
     double n1, n2, n3;
     printf("Enter three different numbers: ");
     scanf("%lf %lf %lf", &n1, &n2, &n3);
-
+    // demo
     // if n1 is greater than both n2 and n3, n1 is the largest
     if (n1 >= n2 && n1 >= n3)
         printf("%.2f is the largest number.", n1);
